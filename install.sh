2690fcb0b8a88628184d9a036aeb030bf423dd5c
Lucien George <lucien.george@shopify.com>
2022-02-28 15:50:13 +0000

    installing bat on spin

--- install.sh
+++ install.sh
@@ -5,9 +5,12 @@ CURRENT_DIR=`pwd`
 ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
 mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
 if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
-  echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
   git clone https://github.com/zsh-users/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting
+fi
+
+if ! command -v bat &> /dev/null; then
+  sudo apt-get install -y bat
 fi
 
 cd "$CURRENT_DIR"
